bb75bee2ff6f9e03539ec8cc725062065baafd08
Salvatore Esposito <salvatore.esposito2v@gmail.com>
2025-11-28 15:09:59 -0500

    first commit

---
+++ sub2txt.py
@@ -0,0 +1,39 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+def file_operation(file_path, operation, content= ''):
+    with open(file_path, operation) as file:
+        #launch an error if operation isn't r or w
+        if operation == 'r':
+            return file.read()
+        
+        if operation == 'w':
+            file.write(content)
+        
+
+load_dotenv()
+
+source_language = 'english'
+target_language = 'italian'
+extetion = 'vtt'
+file_name = "example"
+
+llm = ChatGroq(
+    model="openai/gpt-oss-120b",
+    temperature=0,
+    max_tokens=None,
+    reasoning_format="parsed",
+    timeout=None,
+    max_retries=2,
+)
+
+messages = [
+    (
+        "system", f"You are a helpful assistant that translates {source_language.title()} to {target_language.title()}. Translate the user text, convert it from {extetion} into text and return only the text.",
+    ),
+    ("human", file_operation(f"{file_name}.{extetion}", 'r')),
+]
+
+ai_msg = llm.invoke(messages)
+
+file_operation(f"{file_name}.txt", 'w', ai_msg.content)
